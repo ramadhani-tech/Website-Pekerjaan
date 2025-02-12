16b52f28982c2f1832f6fc19723ceebfe5c34e9c
ramadhani-tech <ridersfast4@gmail.com>
2025-02-12 10:20:25 +0700

    Upload kode website

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Marketplace Pekerjaan & Pelatihan AI</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="head">
+        <img src="foto/Logo Gardina Rovena.png" alt="">
+        <h3>Gardina Rovena</h3>
+    </div>
+    <div class="body">
+    <div class="container">
+        <h1>Selamat Datang di <span>Gardina Rovena</span></h1>
+        <p>Kami menghubungkan pekerja dengan berbagai peluang kerja, baik dari klien maupun dari perusahaan kami sendiri. 
+           Temukan proyek yang sesuai dengan keahlianmu atau tingkatkan keterampilan melalui pelatihan yang kami berikan.</p>
+        <div class="options">
+            <a href="#" class="option client">Saya Klien</a>
+            <a href="#" class="option worker">Saya Personel</a>
+        </div>
+        <a href="#" class="pelajari">Pelajari tentang klien dan personel</a>
+    </div>
+</div>
+</body>
+</html>
